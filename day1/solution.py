97188896525ac84b36df822ea155e719486d8764
Damien Lejay <damien@lejay.be>
2025-12-01 14:44:43 +0100

    Solve day1

---
+++ day1/solution.py
@@ -0,0 +1,35 @@
+# Part I
+count = 0
+dial = 50
+
+with open("input.txt") as file:
+    for line in file:
+        num = int(line[1:])
+        if (line[0] == "L"):
+            num = -num
+        dial = (dial + num) % 100
+        if (dial == 0):
+            count += 1
+
+print(f"The password is {count}")
+
+# Part 2
+count = 0
+dial = 50
+
+with open("input.txt") as file:
+    for line in file:
+        num = int(line[1:])
+        if (line[0] == "R"):
+            count += (dial + num) // 100
+            dial = (dial + num) % 100
+        else:
+            # Apply the function 0 → 0
+            #                    n → 100 - n
+            if (dial != 0):
+                count += (100 - dial + num) // 100
+            else:
+                count += num // 100
+            dial = (dial - num) % 100
+
+print(f"Using method 0x434C49434B, the password is {count}")
